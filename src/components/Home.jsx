f42ef0a1b038693683038a58c2edbf7f269236f2
Nicolás Marcantonio <nicolas.marcantonio@almundo.com>
2021-03-04 13:43:41 -0300

    old video

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -185,7 +185,7 @@ const Home = () => {
 
 
       <div style={{  marginTop: '20px', display: 'flex', alignItems: 'center',  justifyContent: 'center' }} ref={sponsorsRef}>
-        <iframe style={{ marginLeft: '35px' , marginTop: '40px'}} align="middle" width="800" height="425" src="https://www.youtube.com/embed/00wVe3vnuv4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe style={{ marginLeft: '35px' , marginTop: '40px'}} align="middle" width="800" height="425" src="https://www.youtube.com/embed/WMq9kWEm2qM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         <Card className={classes.sponsors} style={{border:'none', boxShadow:'none'}} >
           <CardContent className={classes.content}>
             <Typography gutterBottom className={classes.sponsorsTitle}>
